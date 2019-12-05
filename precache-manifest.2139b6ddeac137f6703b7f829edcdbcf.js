a6c250a7eb32ceebcbeda0024c5693f30633a6bf
Jomark Lopez <lopez.jomark@gmail.com>
2019-12-05 20:43:10 +0800

    Updates

---
+++ precache-manifest.2139b6ddeac137f6703b7f829edcdbcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1385c58d41ef337384838379cc044ff",
+    "url": "/simple-react-calculator/index.html"
+  },
+  {
+    "revision": "ffc42d60c1903436a3c5",
+    "url": "/simple-react-calculator/static/css/main.f3a7e8ba.chunk.css"
+  },
+  {
+    "revision": "f516680777834ad49477",
+    "url": "/simple-react-calculator/static/js/2.ad31d1c7.chunk.js"
+  },
+  {
+    "revision": "ffc42d60c1903436a3c5",
+    "url": "/simple-react-calculator/static/js/main.caa6c8e8.chunk.js"
+  },
+  {
+    "revision": "25fe013e97b8500b43c4",
+    "url": "/simple-react-calculator/static/js/runtime-main.b624922b.js"
+  }
+]);
